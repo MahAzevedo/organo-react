8bb24621f095028e5fe349834d3678af512f443e
Mah <madavetto@gmail.com>
2023-08-16 20:43:29 -0300

    modified: src/componentes

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import logo from './logo.svg';
 import './App.css';
+import Banner from './componentes/Banner/Banner';
 
 function App() {
   return (
     <div className="App">
+      <Banner />
+      <h1>Oi Mádjan</h1>
       <header className="App-header">
+      <h1>Oi Mádjan</h1>
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload. S2 Madi e Tavinho S2 
---
+++ src/componentes/Banner/Banner.js
@@ -0,0 +1,12 @@
+import './Banner.css'
+
+function Banner() {
+    // JSX
+    return (
+        <header>
+            <img src="/img/banner.png" alt="Banner Organo"/>
+        </header>
+    );
+}
+
+export default Banner;
