5c3ebf86a95d99bdb8120d224a4222777f19ebab
Mah <madavetto@gmail.com>
2023-08-17 23:16:33 -0300

    odified: componentes Banner

--- src/componentes/Banner/Banner.js
+++ src/componentes/Banner/Banner.js
@@ -1,6 +1,6 @@
 import './Banner.css'
 
-function Banner() {
+export const Banner = () => {
     // JSX
     return (
         <header className="banner">
@@ -9,4 +9,9 @@ function Banner() {
     );
 }
 
-export default Banner;
+// 2 tipos diferentes de fazer: 1: export const, e; 
+// 2: function tradidional como em CampoTexto, ficam os dois exemplos
+
+// no 2, muda aqui no Banner.js, que importa do index.js do Banner e altera no App.js que
+// é o principal
+// Atenção a essas mudanças :)
