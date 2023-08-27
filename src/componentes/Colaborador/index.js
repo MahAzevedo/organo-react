76ab6de4285af9e00d3a5a72bc637ec96b18d741
Mah <madavetto@gmail.com>
2023-08-26 23:18:16 -0300

    modified:   src/componentes/Colaborador/index.js

--- src/componentes/Colaborador/index.js
+++ src/componentes/Colaborador/index.js
@@ -2,8 +2,12 @@ import './Colaborador.css'
 
 const Colaborador = () => {
     return (
-
+        <div>
+            <img src='' alt=''/>
+        </div>
     )
 }
 
 export default Colaborador;
+
+// 
