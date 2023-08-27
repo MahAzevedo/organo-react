e8dd195cb4f0cdd488ca7ac2ee5a3d15ddccb7c8
Mah <madavetto@gmail.com>
2023-08-27 18:24:15 -0300

    modified:   src/componentes/Colaborador/Colaborador.css

--- src/componentes/Colaborador/index.js
+++ src/componentes/Colaborador/index.js
@@ -2,15 +2,17 @@ import './Colaborador.css'
 
 const Colaborador = () => {
     return (
-        <div>
-            <img src='' alt=''/>
-        </div>
-        <div>
-            
+        <div className='colaborador'>
+            <div className='cabecalho'>
+                <img src='https://github.com/MahAzevedo.png' alt='Mádjan Azevedo' />
+            </div>
+            <div className='rodape'>
+                <h4>Mádjan Azevedo</h4>
+                <h5>Front-end</h5>
+            </div>
         </div>
     )
 }
 
 export default Colaborador;
 
-// 
