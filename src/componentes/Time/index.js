d820de1120379a38561c7faed91bc7fb208f855d
Mah <madavetto@gmail.com>
2023-08-28 13:11:52 -0300

    modified:   src/componentes/Time/index.js

--- src/componentes/Time/index.js
+++ src/componentes/Time/index.js
@@ -7,7 +7,7 @@ const Time = (props) => {
     return (
         <section className='time' style={css}> 
             <h3 style={{borderColor: props.corPrimaria}}>{props.nome}</h3>
-            <Colaborador />
+            
         </section>
     )
 }
