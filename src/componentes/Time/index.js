11f8a712ec207e77f9f154e7362e1911e23d9a2f
Mah <madavetto@gmail.com>
2023-08-28 16:59:33 -0300

    modified:   src/componentes/Time/index.js

--- src/componentes/Time/index.js
+++ src/componentes/Time/index.js
@@ -5,13 +5,15 @@ const Time = (props) => {
     const css = { backgroundColor: props.corSecundaria }
 
     return (
-        <section className='time' style={css}> 
-            <h3 style={{borderColor: props.corPrimaria}}>{props.nome}</h3>
-            {props.colaboradores.map(colaborador => <Colaborador 
-                nome={colaborador.nome}
-                cargo={colaborador.cargo}
-                imagem={colaborador.imagem}
-            />)}
+        <section className='time' style={css}>
+            <h3 style={{ borderColor: props.corPrimaria }}>{props.nome}</h3>
+            <div className='colaboradores'>
+                {props.colaboradores.map(colaborador => <Colaborador
+                    nome={colaborador.nome}
+                    cargo={colaborador.cargo}
+                    imagem={colaborador.imagem}
+                />)}
+            </div>
         </section>
     )
 }
